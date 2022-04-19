0603a064376e6c17a700a9d4e8065d75f29ca9d8
TEN  ALEXANDER <35337698+sashaTen@users.noreply.github.com>
2022-04-19 21:19:54 +0900

    created  the  bluprint  for the  header  tag

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./styles.css";
+import { BsFillCaretDownFill, BsFillCartFill } from "react-icons/bs";
 import { useState, useEffect } from "react";
 import axios from "axios";
 export default function App() {
@@ -9,17 +10,21 @@ export default function App() {
       .get("https://course-api.com/react-store-products/")
       .then((products) => {
         setItems(products.data);
-        console.log(products.data);
       });
   }, []);
 
   return (
     <div className="App">
-      {items.map((item) => {
-        return <div>{item.name}</div>;
-      })}
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <header className="header">
+        <p className="latestNews">
+          latest news : <span> get new year discount 20%</span>
+        </p>
+
+        <BsFillCaretDownFill />
+        <BsFillCartFill />
+        <p>Settings</p>
+        <p>Account</p>
+      </header>
     </div>
   );
 }
