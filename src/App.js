17091f0e54369bd50aadd0c82849dadbb6c3603d
TEN  ALEXANDER <35337698+sashaTen@users.noreply.github.com>
2022-04-24 04:47:29 +0900

    made auto slide of billBoard images

--- src/App.js
+++ src/App.js
@@ -13,7 +13,10 @@ export default function App() {
   const [moveApp, setMoveApp] = useState("App");
   const imgUrl1 = `https://cgaxisimages.fra1.cdn.digitaloceanspaces.com/2018/07/cgaxis_models_106_14a.jpg`;
   const imgUrl2 = `https://shop.mohd.it/media/catalog/product/cache/11b1b7f5d04077d123613fe5bd9b4f06/l/o/lovy-sofa-bonaldo.jpg`;
-  const imgUrl3 = `https://mir-s3-cdn-cf.behance.net/project_modules/fs/cbeffe68752533.5b686b9a7f5f2.jpg`;
+  const imgUrl3 = `https://secure.img1-fg.wfcdn.com/im/83591330/compr-r85/1182/118254953/sonny-91-sofa-with-reversible-cushions.jpg`;
+  let billBoardIndex = 0;
+  const moveLeft = [40, -40, -120];
+  const [moveImage, setMoveImage] = useState(moveLeft[billBoardIndex] + "%");
 
   useEffect(() => {
     axios
@@ -22,6 +25,20 @@ export default function App() {
         setItems(products.data);
         console.log(products.data);
       });
+  }, []);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      billBoardIndex++;
+      if (billBoardIndex > 2) {
+        billBoardIndex = 0;
+      }
+      setMoveImage(moveLeft[billBoardIndex] + "%");
+      console.log(billBoardIndex);
+
+      // setMoveImage()
+    }, 2000);
+
+    return () => clearInterval(interval);
   }, []);
 
   return (
@@ -95,8 +112,12 @@ export default function App() {
         </p>
       </section>
       <BsSearch className="searchIcon" />
-
-      <img className="billBoard" src={imgUrl3} />
+      <section className="imgContainer" style={{ left: moveImage }}>
+        <img className="billBoard" src={imgUrl1} />
+        <img className="billBoard" src={imgUrl2} />
+        <img className="billBoard" src={imgUrl3} />
+      </section>
+      <img className="billBoard" src={imgUrl1} />
       <div className=""></div>
 
       <section className="galleryContainer">
