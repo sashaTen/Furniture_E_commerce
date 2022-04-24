5ae3f9785d8b192eebbcdb2ca85d260c4d959a8a
TEN  ALEXANDER <35337698+sashaTen@users.noreply.github.com>
2022-04-24 10:44:35 +0900

    created menu  component and  imported  functionalities there

---
+++ src/menu.js
@@ -0,0 +1,47 @@
+import { BsFillCaretDownFill, BsSearch } from "react-icons/bs";
+import { AiOutlineMenu } from "react-icons/ai";
+
+const Menu = (props) => {
+  return (
+    <>
+      <AiOutlineMenu
+        className="menuIcon"
+        onClick={() => {
+          props.changeClass("moveApp");
+        }}
+      />
+      <section className="menuContainer">
+        <p
+          className="btnXmobileMenu"
+          onClick={() => {
+            props.changeClass("App");
+          }}
+        >
+          {" "}
+          X{" "}
+        </p>
+        <p className="btn">
+          Home <BsFillCaretDownFill />{" "}
+        </p>
+        <p className="btn">
+          Collection <BsFillCaretDownFill />{" "}
+        </p>
+        <p className="btn">
+          Category
+          <BsFillCaretDownFill />{" "}
+        </p>
+        <p className="btn">
+          About Us <BsFillCaretDownFill />{" "}
+        </p>
+        <p className="btn">
+          Blog <BsFillCaretDownFill />{" "}
+        </p>
+        <p className="btn">
+          Contact <BsFillCaretDownFill />{" "}
+        </p>
+      </section>
+    </>
+  );
+};
+
+export default Menu;
