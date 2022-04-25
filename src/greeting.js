b21f67e1ba7825120f50c9f53237708b22fbfe60
TEN  ALEXANDER <35337698+sashaTen@users.noreply.github.com>
2022-04-25 18:41:27 +0900

    finished  styling greetings

--- src/greeting.js
+++ src/greeting.js
@@ -3,13 +3,15 @@ import { BsFillStarFill } from "react-icons/bs";
 
 const Greet = () => {
   return (
-    <div>
-      Greeetings
-      <BsFillStarFill className="starIcon" />
+    <div className="greetContainer">
+      <h1 className="trustStyle">TRUST YOUR STYLE TO</h1>
+      <h1 className="conosin">CONOSIN</h1>
       <img
         className="newSeason"
         src="https://k4j3j2s7.rocketcdn.me/furnob/wp-content/uploads/2022/01/season-text.png"
       />
+
+      <button className="GreetBtn">Shop Colletion</button>
     </div>
   );
 };
