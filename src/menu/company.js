ccc4a76ad0a8a259c07351a7cd0eb5a4c587298a
TEN  ALEXANDER <35337698+sashaTen@users.noreply.github.com>
2022-04-25 17:54:29 +0900

    set  of  unique companies  now  is  displayed

--- src/menu/company.js
+++ src/menu/company.js
@@ -1,5 +1,20 @@
-const Company = () => {
-  return <div>Company</div>;
+const Company = (props) => {
+  const items = props.items;
+  let companies = [];
+  for (let i = 0; i < items.length; i++) {
+    companies.push(items[i].company);
+  }
+  let uniqueCompanies = [...new Set(companies)];
+
+  console.log(uniqueCompanies);
+
+  return (
+    <div>
+      {uniqueCompanies.map((item) => {
+        return <div>{item}</div>;
+      })}
+    </div>
+  );
 };
 
 export default Company;
