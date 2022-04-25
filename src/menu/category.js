76e09ad77b9f04ff5ecfc371fe24091bfa8eb7f5
TEN  ALEXANDER <35337698+sashaTen@users.noreply.github.com>
2022-04-25 16:40:19 +0900

    now  by clickiing the btn 'category' unique  categories  will  be  displayed

--- src/menu/category.js
+++ src/menu/category.js
@@ -1,12 +1,19 @@
 const Category = (props) => {
   const items = props.items;
-  console.log(items);
+  let categories = [];
+  for (let i = 0; i < items.length; i++) {
+    categories.push(items[i].category);
+  }
+  //let uniqueChars = [...new Set(chars)];
+  let uniqueCategories = [...new Set(categories)];
+  console.log(uniqueCategories);
 
   return (
-    <>
-      Category
-      <div></div>
-    </>
+    <div className="categoryContainer">
+      {uniqueCategories.map((item) => {
+        return <p>{item}</p>;
+      })}
+    </div>
   );
 };
 
