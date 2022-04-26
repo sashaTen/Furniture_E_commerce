325e7c3bcc05ac20cf3829354702e1ce1d5cbc64
TEN  ALEXANDER <35337698+sashaTen@users.noreply.github.com>
2022-04-26 20:34:16 +0900

    chnaged file  folder of  greering

--- src/slideAndGreeting/slideGallery.js
+++ src/slideAndGreeting/slideGallery.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 
 const imgUrl1 = `https://cgaxisimages.fra1.cdn.digitaloceanspaces.com/2018/07/cgaxis_models_106_14a.jpg`;
-const imgUrl2 = `https://shop.mohd.it/media/catalog/product/cache/11b1b7f5d04077d123613fe5bd9b4f06/l/o/lovy-sofa-bonaldo.jpg`;
+const imgUrl2 = `https://htdeco.fr/2034-thickbox_default/baroque-white-and-silver-sofa.jpg`;
 const imgUrl3 = `https://secure.img1-fg.wfcdn.com/im/83591330/compr-r85/1182/118254953/sonny-91-sofa-with-reversible-cushions.jpg`;
 
 const SlideGallery = () => {
