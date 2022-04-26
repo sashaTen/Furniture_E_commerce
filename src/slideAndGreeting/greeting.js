3bf97bc6910ea7f489e63681a973f5bfed757628
TEN  ALEXANDER <35337698+sashaTen@users.noreply.github.com>
2022-04-26 20:56:20 +0900

    finished the greeting position

--- src/slideAndGreeting/greeting.js
+++ src/slideAndGreeting/greeting.js
@@ -5,15 +5,16 @@ const Greet = () => {
   return (
     <div className="greetContainer">
       <h1 className="trustStyle">TRUST YOUR STYLE TO CONOSIN</h1>
+
+      <img
+        className="newSeason"
+        src="https://k4j3j2s7.rocketcdn.me/furnob/wp-content/uploads/2022/01/season-text.png"
+      />
       <p className="pTagUnderGreet">
         Ut lobortis, ex vitae lobortis cursus, elit nisi facilisis urna, at
         porttitor eros leo ac ex. Nunc molestie turpis varius purus accumsan
         maximus. Nam ut libero aliquet, consequat ipsum sit amet, aliquet odio
       </p>
-      <img
-        className="newSeason"
-        src="https://k4j3j2s7.rocketcdn.me/furnob/wp-content/uploads/2022/01/season-text.png"
-      />
 
       <button className="GreetBtn">Shop Colletion</button>
     </div>
