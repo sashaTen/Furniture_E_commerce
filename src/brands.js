cbbd6851d57baf373076e08d57e3ec7e1f37f8e0
TEN  ALEXANDER <35337698+sashaTen@users.noreply.github.com>
2022-04-26 20:46:15 +0900

    fixed  greeeting  compnent

--- src/brands.js
+++ src/brands.js
@@ -1,5 +1,24 @@
 const Brands = () => {
-  return <div>Brands</div>;
+  const imgUrls = [
+    "https://k4j3j2s7.rocketcdn.me/furnob/wp-content/uploads/2021/12/logo-1.png",
+    "https://k4j3j2s7.rocketcdn.me/furnob/wp-content/uploads/2021/12/logo-8.png",
+    "https://k4j3j2s7.rocketcdn.me/furnob/wp-content/uploads/2021/12/logo-7.png",
+    "https://k4j3j2s7.rocketcdn.me/furnob/wp-content/uploads/2021/12/logo-5.png",
+    "https://k4j3j2s7.rocketcdn.me/furnob/wp-content/uploads/2021/12/logo-2.png",
+    "https://k4j3j2s7.rocketcdn.me/furnob/wp-content/uploads/2021/12/logo-2.png "
+  ];
+
+  return (
+    <div>
+      {imgUrls.map((item) => {
+        return (
+          <div>
+            <img src={item} />
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Brands;
--- src/slideAndGreeting/greeting.js
+++ src/slideAndGreeting/greeting.js
@@ -4,8 +4,12 @@ import { BsFillStarFill } from "react-icons/bs";
 const Greet = () => {
   return (
     <div className="greetContainer">
-      <h1 className="trustStyle">TRUST YOUR STYLE TO</h1>
-      <h1 className="conosin">CONOSIN</h1>
+      <h1 className="trustStyle">TRUST YOUR STYLE TO CONOSIN</h1>
+      <p className="pTagUnderGreet">
+        Ut lobortis, ex vitae lobortis cursus, elit nisi facilisis urna, at
+        porttitor eros leo ac ex. Nunc molestie turpis varius purus accumsan
+        maximus. Nam ut libero aliquet, consequat ipsum sit amet, aliquet odio
+      </p>
       <img
         className="newSeason"
         src="https://k4j3j2s7.rocketcdn.me/furnob/wp-content/uploads/2022/01/season-text.png"
