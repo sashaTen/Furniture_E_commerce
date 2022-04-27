5f58ba1132ed149edc1b925593320b91ea6cef3a
TEN  ALEXANDER <35337698+sashaTen@users.noreply.github.com>
2022-04-27 20:04:13 +0900

    fnished  skeleton  for the home   body   component

--- src/components/homePageBody.js
+++ src/components/homePageBody.js
@@ -1,5 +1,51 @@
+//BsArrowRight
+import { BsArrowRight } from "react-icons/bs";
+
 const HomePageBody = () => {
-  return <div>hello home page</div>;
+  const imgUrl1 =
+    "https://dl.airtable.com/.attachmentThumbnails/1e4a818f5184993e430420a152315b40/873c7094?ts=1651055573&userId=usrQMwWEPx18KgLcP&cs=b17d245a2110110e";
+  const imgUrl2 =
+    "https://dl.airtable.com/.attachmentThumbnails/89ba7458c24252be77f5a835dd398880/c13ef359?ts=1651055573&userId=usrQMwWEPx18KgLcP&cs=27d5098576ec5938";
+  const imgUrl3 =
+    "https://dl.airtable.com/.attachmentThumbnails/e3fa7aa6dc112c4998da18bb401bd70f/61e2fb5e?ts=1651055573&userId=usrQMwWEPx18KgLcP&cs=8b1787f55cd7d1db";
+
+  return (
+    <div className="homePagebodyContainer">
+      <section className="collectionAnnouncement">
+        <p>Massey Collection 2021</p>
+        <h2>New Decoration For Your</h2>
+        <h2>Living Spaces</h2>
+        <p>Praesent faucibus, lorem ut sollicitudin dapibus,</p>
+        <p> quam ligula pretium magna, eu congue ex quam ut neque.</p>
+        <p>It'll be worth the wait very soon !</p>
+        <h2>2022 - 07- 21 </h2>
+      </section>
+      <section className="img1Container">
+        <img className="homePageImg" src={imgUrl1} />
+        <h3>Deluxe Venezie Collection</h3>
+        <p>
+          {" "}
+          shop collection <BsArrowRight />{" "}
+        </p>
+      </section>
+      <section className="img2Container">
+        <img src={imgUrl2} className="homePageImg" />
+        <h3>Great Design For Home and Decoration.</h3>
+        <p>
+          {" "}
+          shop collection <BsArrowRight />{" "}
+        </p>
+      </section>
+      <section className="img3Container">
+        <img src={imgUrl3} className="homePageImg" />
+        <h3>Le’Bologna Collection</h3>
+        <p>
+          {" "}
+          shop collection <BsArrowRight />{" "}
+        </p>
+      </section>
+    </div>
+  );
 };
 
 export default HomePageBody;
