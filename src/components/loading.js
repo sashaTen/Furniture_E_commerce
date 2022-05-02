c560d1c79dc0236048c2f2b7e01dc722e371259f
TEN  ALEXANDER <35337698+sashaTen@users.noreply.github.com>
2022-05-02 14:35:09 +0900

    finished   logging   component

--- src/components/loading.js
+++ src/components/loading.js
@@ -1,5 +1,12 @@
+import { BsFillCartFill } from "react-icons/bs";
+
 const Loading = () => {
-  return <div className="loadingContainer"></div>;
+  return (
+    <div className="loadingContainer">
+      <BsFillCartFill className="loadingCart" />
+      <section className="loadingMovingSquare"></section>
+    </div>
+  );
 };
 
 export default Loading;
