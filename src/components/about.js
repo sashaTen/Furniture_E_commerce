80ebe3f5c6f7e59e0e2d38213ea40e34f9a7d6c1
TEN  ALEXANDER <35337698+sashaTen@users.noreply.github.com>
2022-05-06 09:23:33 +0900

    finished  about  component

--- src/components/about.js
+++ src/components/about.js
@@ -1,8 +1,26 @@
 const About = () => {
+  const imgUrls = [
+    "https://react-course-comfy-sloth-store.netlify.app/static/media/hero-bcg.a876f19f.jpeg",
+    "https://react-course-comfy-sloth-store.netlify.app/static/media/hero-bcg-2.78991864.jpeg"
+  ];
   return (
     <>
-      About page
-      <div className="aboutContainer"></div>
+      <div className="aboutContainer">
+        <div className="aboutOrangaContainer"></div>
+        <img className="aboutImg" src={imgUrls[0]} />
+        <img className="aboutImg2" src={imgUrls[1]} />
+        <section className="aboutSection">
+          <h3> about us </h3>
+          <p className="aboutDescription">
+            Here I was, interviewing the architect Witold Rybczynski about his
+            new book, an appreciation of the chair and its 5,000-year history,
+            and I was doing it from a standing desk. Nearby, I had a perfectly
+            tolerable chair, with snazzy features like a mesh-fabric seat,
+            pneumatic seat-height adjustment, and polyurethane armrests. But it
+            wasn’t looking so appealing, perhaps because the
+          </p>
+        </section>
+      </div>
     </>
   );
 };
