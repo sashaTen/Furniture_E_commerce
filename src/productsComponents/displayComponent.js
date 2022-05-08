7c668eff5d6db6f7bcb110e50ca37843a225bce5
TEN  ALEXANDER <35337698+sashaTen@users.noreply.github.com>
2022-05-08 09:04:49 +0900

    checked  how  to  pass dtaa  throught  link  and  how will it  affect the items of products

--- src/productsComponents/displayComponent.js
+++ src/productsComponents/displayComponent.js
@@ -2,13 +2,27 @@ import data from "../components/data";
 import { useState, useEffect } from "react";
 import SingleItem from "./displaySingleComponent";
 import Loading from "../components/loading";
+import { useLocation } from "react-router-dom";
+
 const DisplayComponent = () => {
-  let items = data;
+  const [items, setItems] = useState([]);
+  const location = useLocation();
+  const state = location.state;
+  useEffect(() => {
+    if (state) {
+      console.log(state);
+    } else if (!state) {
+      setItems(data);
+    }
+  }, []);
+
+  console.log(state);
+
   const [showLoading, setShowLoading] = useState(true);
   useEffect(() => {
     const timer = setTimeout(() => {
       setShowLoading(false);
-    }, 1300);
+    }, 900);
     return () => clearTimeout(timer);
   }, []);
   if (showLoading) {
