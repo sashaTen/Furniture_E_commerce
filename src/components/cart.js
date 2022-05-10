2655da8ee63e39ba90ed130ae7438c134f3ebc33
TEN  ALEXANDER <35337698+sashaTen@users.noreply.github.com>
2022-05-10 20:58:44 +0900

    made  the  styling  for the cart

--- src/components/cart.js
+++ src/components/cart.js
@@ -1,8 +1,12 @@
 import Loading from "./loading";
 import { useState, useEffect } from "react";
-
+import data from "./data";
 const Cart = () => {
   const [showLoading, setShowLoading] = useState(true);
+  let cartItems = [];
+  for (let i = 0; i < 4; i++) {
+    cartItems.push(data[i]);
+  }
   useEffect(() => {
     const timer = setTimeout(() => {
       setShowLoading(false);
@@ -12,7 +16,33 @@ const Cart = () => {
   if (showLoading) {
     return <Loading />;
   }
-  return <>this is the cart component</>;
+  return (
+    <div className="cartComponent">
+      your cart
+      <div className="cartContainer">
+        {cartItems.map((item) => {
+          return (
+            <div className="cartItemContainer">
+              <img className="cartImg" src={item.image} />
+              <div>
+                <p className="cartItemName"> {item.name}</p>
+                <p className="cartItemPrice">{item.price / 100}$</p>
+              </div>
+              <section className="cartPlusMinus">
+                <button className="cartBtnPlus">+</button>
+                <h4>1</h4>
+                <button className="cartBtnMinus">-</button>
+              </section>
+            </div>
+          );
+        })}
+      </div>
+      <section className="cartBottom">
+        <button className="cartBuyBtn"> buy </button>
+        <h3>total</h3>
+      </section>
+    </div>
+  );
 };
 
 export default Cart;
