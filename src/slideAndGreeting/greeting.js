a8e6a189befdc03e31cf95147cce954238bfc141
TEN  ALEXANDER <35337698+sashaTen@users.noreply.github.com>
2022-05-18 18:40:51 +0900

    deleted   slides  of  sofa  and  css classes  related to  them.  also   deleted the greeeting

--- src/slideAndGreeting/greeting.js
+++ src/slideAndGreeting/greeting.js
@@ -4,17 +4,7 @@ import { AiOutlineArrowRight } from "react-icons/ai";
 const Greet = () => {
   return (
     <div className="greetContainer">
-      <h1 className="trustStyle">TRUST YOUR STYLE TO CONOSIN</h1>
-
-      <img
-        className="newSeason"
-        src="https://k4j3j2s7.rocketcdn.me/furnob/wp-content/uploads/2022/01/season-text.png"
-      />
-      <Link to="/items">
-        <button className="GreetBtn">
-          start shopping <AiOutlineArrowRight />
-        </button>
-      </Link>
+      <Link to="/items"></Link>
     </div>
   );
 };
--- src/slideAndGreeting/slideGallery.js
+++ src/slideAndGreeting/slideGallery.js
@@ -5,37 +5,7 @@ const imgUrl2 = `https://htdeco.fr/2034-thickbox_default/baroque-white-and-silve
 const imgUrl3 = `https://secure.img1-fg.wfcdn.com/im/83591330/compr-r85/1182/118254953/sonny-91-sofa-with-reversible-cushions.jpg`;
 
 const SlideGallery = () => {
-  useEffect(() => {
-    const interval = setInterval(() => {
-      billBoardIndex++;
-      if (billBoardIndex > 2) {
-        billBoardIndex = 0;
-      }
-      setMoveImage(moveLeft[billBoardIndex] + "%");
-
-      // setMoveImage()
-    }, 2000);
-
-    return () => clearInterval(interval);
-  }, []);
-
-  let billBoardIndex = 0;
-  const moveLeft = [40, -40, -120];
-  const [moveImage, setMoveImage] = useState(moveLeft[billBoardIndex] + "%");
-  return (
-    <>
-      <section className="imgContainer" style={{ left: moveImage }}>
-        <img className="billBoard" src={imgUrl1} />
-        <img className="billBoard" src={imgUrl2} />
-        <img className="billBoard" src={imgUrl3} />
-      </section>
-      <section className="imgContainerMobile">
-        <img className="billBoardMobile" src={imgUrl1} />
-        <img className="billBoardMobile" src={imgUrl2} />
-        <img className="billBoardMobile" src={imgUrl3} />
-      </section>
-    </>
-  );
+  return <></>;
 };
 
 export default SlideGallery;
