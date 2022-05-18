c85eeaae2b17540758241555c587ca688e633783
TEN  ALEXANDER <35337698+sashaTen@users.noreply.github.com>
2022-05-18 23:19:00 +0900

    added  greet  text  over   shop  start   button

--- src/slideAndGreeting/greeting.js
+++ src/slideAndGreeting/greeting.js
@@ -3,11 +3,20 @@ import { BsFillStarFill } from "react-icons/bs";
 import { AiOutlineArrowRight } from "react-icons/ai";
 const Greet = () => {
   return (
-    <div className="greetContainer">
-      <Link className="greetLinkToItems" to="/items">
-        start shopping
-      </Link>
-    </div>
+    <>
+      <div className="greetContainer">
+        <section className="greetMessage">
+          exprience
+          <h3 style={{ color: "lightblue" }}>fast</h3>
+          and <h3 style={{ color: "red" }}>reliable</h3>
+          <p>shopping</p>
+        </section>
+
+        <Link className="greetLinkToItems" to="/items">
+          start shopping
+        </Link>
+      </div>
+    </>
   );
 };
 
