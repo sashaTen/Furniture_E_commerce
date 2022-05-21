ccfe399f91c550a431ced91d128981200af28706
TEN  ALEXANDER <35337698+sashaTen@users.noreply.github.com>
2022-05-21 21:54:03 +0900

    if  login global  state  true then  it  return info if  false it returns form

--- src/components/login.js
+++ src/components/login.js
@@ -1,11 +1,20 @@
 import { useContext } from "react";
-import { accountInfo } from "../context";
+import { accountInfo, isLogged } from "../context";
 const Login = () => {
   const personalInfo = useContext(accountInfo);
+  const logged = useContext(isLogged);
+  console.log(logged);
 
+  if (logged.logged) {
+    return (
+      <>
+        <div className="loginContainer">logged in</div>
+      </>
+    );
+  }
   return (
     <>
-      <div className="loginContainer">login page g</div>
+      <div className="loginContainer">not logged in</div>
     </>
   );
 };
