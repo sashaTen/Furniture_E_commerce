1d6eeadc2018e145ffc08952e7bb8157469e1d48
TEN  ALEXANDER <35337698+sashaTen@users.noreply.github.com>
2022-05-21 22:06:18 +0900

    created the form for the login  page

--- src/components/login.js
+++ src/components/login.js
@@ -3,7 +3,6 @@ import { accountInfo, isLogged } from "../context";
 const Login = () => {
   const personalInfo = useContext(accountInfo);
   const logged = useContext(isLogged);
-  console.log(logged);
 
   if (logged.logged) {
     return (
@@ -14,7 +13,46 @@ const Login = () => {
   }
   return (
     <>
-      <div className="loginContainer">not logged in</div>
+      <form>
+        <h3>Register</h3>
+        <p>Please fill in this form to create an account.</p>
+        <div className="loginContainer">
+          <label for="email">
+            <b>Email</b>
+          </label>
+          <input
+            type="text"
+            placeholder="Enter Email"
+            name="email"
+            id="email"
+            required
+          />
+
+          <label for="psw">
+            <b>Password</b>
+          </label>
+          <input
+            type="password"
+            placeholder="Enter Password"
+            name="psw"
+            id="psw"
+            required
+          />
+
+          <label for="adress">
+            <b>ADRESS</b>
+          </label>
+          <input type="password" placeholder="adress" name="adress" required />
+          <button
+            onClick={() => {
+              logged.setLogged(true);
+            }}
+            class="cartBuyBtn"
+          >
+            Register
+          </button>
+        </div>
+      </form>
     </>
   );
 };
