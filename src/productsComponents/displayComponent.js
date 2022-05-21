9e034e02fe9ae19eb35b067c1becc8a450d7f960
TEN  ALEXANDER <35337698+sashaTen@users.noreply.github.com>
2022-05-21 17:56:53 +0900

    deleted  filters component

--- src/productsComponents/displayComponent.js
+++ src/productsComponents/displayComponent.js
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 import SingleItem from "./displaySingleComponent";
 import Loading from "../components/loading";
 import { useLocation } from "react-router-dom";
-import Filters from "./filters";
+
 const DisplayComponent = () => {
   const location = useLocation();
   const state = location.state;
@@ -40,9 +40,6 @@ const DisplayComponent = () => {
   return (
     <>
       <section className="productsPage">
-        <div className="filtersContainer">
-          <Filters />
-        </div>
         <div className="productsContainer">
           {list.map((item) => {
             return <SingleItem {...item} key={item.id} />;
--- src/productsComponents/filters.js
+++
@@ -1,4 +0,0 @@
-const Filters = () => {
-  return <>filter</>;
-};
-export default Filters;
