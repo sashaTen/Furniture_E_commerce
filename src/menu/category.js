033845aada1d4ca62ea317c9537f17728543c4b6
AlexanderTenSeok <sasha1keshten@gmail.com>
2022-09-05 10:18:54 +0000

    checked   push

--- src/menu/category.js
+++ src/menu/category.js
@@ -1,4 +1,4 @@
-//import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { moveAppContext } from "../context";
 const Category = (props) => {
